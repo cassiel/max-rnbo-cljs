da4a60a0ca02ca83e46b7e82764be70f07fcbfc4
Nick (Git client) <nick@cassiel.com>
2023-05-10 11:18:29 +0100

    Version bumps.

--- net.cassiel.max-rnbo/project.clj
+++ net.cassiel.max-rnbo/project.clj
@@ -6,8 +6,8 @@
 
   :min-lein-version "2.7.1"
 
-  :dependencies [[org.clojure/clojure "1.10.0"]
-                 [org.clojure/clojurescript "1.11.4"]
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [org.clojure/clojurescript "1.11.60"]
                  [org.clojure/core.async "1.6.673"]
                  [binaryage/oops "0.7.2"]
                  [com.stuartsierra/component "1.1.0"]
@@ -15,7 +15,7 @@
 
   :source-paths ["src"]
 
-  :plugins [[lein-environ "1.0.0"]
+  :plugins [[lein-environ "1.2.0"]
             [com.github.liquidz/antq "RELEASE"]
             [lein-cljsbuild "1.1.8"]]
 
@@ -23,8 +23,7 @@
             "fig:min"   ["run" "-m" "figwheel.main" "-O" "advanced" "-bo" "dev"]
             "fig:test"  ["run" "-m" "figwheel.main" "-co" "test.cljs.edn" "-m" "net.test-runner"]}
 
-  :profiles {:dev {:dependencies [[com.bhauman/figwheel-main "0.2.17"]
-                                  [org.slf4j/slf4j-nop "1.7.30"]
+  :profiles {:dev {:dependencies [[com.bhauman/figwheel-main "0.2.18"]
                                   [com.bhauman/rebel-readline-cljs "0.1.4"]]
 
                    :resource-paths ["target"]
